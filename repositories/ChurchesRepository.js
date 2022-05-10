45d29fa7c486493842fe570cc75185de809ee51b
projet-eglise <projet-eglise@protonmail.com>
2022-05-10 17:50:40 +0200

    PE-25 - Change route for churches.

--- repositories/ChurchesRepository.js
+++ repositories/ChurchesRepository.js
@@ -3,7 +3,7 @@ const resource = '/church'
 export default ($axios) => {
     return ({
         async getAllForJoin() {
-            return await $axios.get(resource + 'es')
+            return await $axios.get(resource + 'es/forJoin')
         },
         async add(payload) {
             return await $axios.post(resource + '/add', payload)
