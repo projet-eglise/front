01782d1cf9b951c18cfd7486505b8c997e3fc6ec
projet-eglise <projet-eglise@protonmail.com>
2022-07-23 18:06:12 +0200

    PE-69 - Refactoring login page.

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-col class="login-form ml-auto mr-auto" height="100%">
-    <AppLogo centred />
+  <v-col class="mb-4em ml-auto mr-auto" height="100%">
+    <AppLogo class="mb-8" centred />
 
     <v-form ref="form" @submit="connect">
       <AppTextFieldEmail v-model="email" />
       <AppTextFieldPassword v-model="password" />
 
-      <AppButtonBlock type="submit" :loading="isLoading">S'identifier</AppButtonBlock>
+      <AppButtonBlock type="submit" :loading="isLoading">{{ $t('authentication.login') }}</AppButtonBlock>
     </v-form>
   </v-col>
 </template>
@@ -17,10 +17,11 @@ export default {
   meta: { protected: false },
   data() {
     return {
-      email: '',
-      password: '',
+      email: process.env.DEFAULT_LOGIN ?? '',
+      password: process.env.DEFAULT_PASSWORD ?? '',
       showPassword: false,
       isLoading: false,
+      wantOpenDialog: false,
     }
   },
   mounted() {
@@ -32,37 +33,28 @@ export default {
       event.stopPropagation()
 
       if (this.$refs.form.validate()) {
-        this.$store.dispatch('components/alert-component/hide')
         this.isLoading = true
-
-        try {
-          const res = await this.$repositories.authentication.login(this.email, this.password)
-          const { status, data } = res
-
-          if (status === 200 && data.message && data.data && data.data.token) {
-            this.$store.dispatch('authentication/login', data.data.token)
-            if (this.$store.getters['main/referer'] !== '') this.$router.push(this.$store.getters['main/referer'])
-            else this.$router.push('/admin/dashboard')
-          } else {
-            this.$store.dispatch('authentication/logout')
-          }
-
-          this.isLoading = false
-        } catch (error) {
-          this.$store.dispatch('components/alert-component/displayError', error.response.data.error)
-          this.isLoading = false
-        }
+        await this.$authentication.adminLogin(this.email, this.password)
+        this.isLoading = false
       }
+    },
+    openResetPassword(event) {
+      event.preventDefault()
+      event.stopPropagation()
+
+      this.wantOpenDialog = true
+      setTimeout(
+        function () {
+          this.wantOpenDialog = false
+        }.bind(this),
+        10
+      )
     },
   },
 }
 </script>
 
 <style scoped>
-.login-form {
-  margin-bottom: 4em;
-}
-
 .center-v-alert {
   left: 50%;
   transform: translate(-50%, 0);
