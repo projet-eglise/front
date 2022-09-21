4b5cd663911dcab6f08519b9a2f36c9de6ab0940
projet-eglise <projet-eglise@protonmail.com>
2022-09-21 21:36:44 +0200

    Fix mobile size

--- components/AppBarAdmin.vue
+++ components/AppBarAdmin.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="absolute top-0 left-0 bg-[#FFFFFF] h-20 w-screen flex items-center px-4 rounded-br-lg rounded-bl-lg">
+  <div class="absolute top-0 left-0 bg-[#FFFFFF] h-14 w-screen flex items-center px-4 rounded-br-lg rounded-bl-lg">
     <div @click="open">
       <v-icon>fas fa-bars</v-icon>
     </div>
-    <AppLogo class="h-14" centred />
+    <AppLogo class="h-12" centred />
     <div ref="drawerContainer" class="hidden">
       <AppNavigationDrawerAdminMobile ref="drawer" />
       <div class="absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.5)]" @click="close"></div>
